a13a29e0b326cfbac8d422ae71f2bfbbdf685ce3
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2024-05-24 02:29:47 +0300

    26

--- puppet_Part/simplePuppet/plans/install_nginx.pp
+++ puppet_Part/simplePuppet/plans/install_nginx.pp
@@ -1,7 +1,7 @@
 plan simple_puppet_bolt::install_nginx (
   TargetSpec $nodes,
 ) {
-  run_task('package', $nodes, {'action' => 'install', 'name' => 'nginx'})
-  run_task('service', $nodes, {'action' => 'start', 'name' => 'nginx'})
-  run_task('service', $nodes, {'action' => 'enable', 'name' => 'nginx'})
+  run_task('package', $nodes, {'action' => 'install', 'name' => 'nginx'}, 'become' => true)
+  run_task('service', $nodes, {'action' => 'start', 'name' => 'nginx'}, 'become' => true)
+  run_task('service', $nodes, {'action' => 'enable', 'name' => 'nginx'}, 'become' => true)
 }
--- puppet_Part/simplePuppet/plans/revert_nginx.pp
+++ puppet_Part/simplePuppet/plans/revert_nginx.pp
@@ -1,7 +1,7 @@
 plan simple_puppet_bolt::remove_nginx (
   TargetSpec $nodes,
 ) {
-  run_task('service', $nodes, {'action' => 'stop', 'name' => 'nginx'})
-  run_task('service', $nodes, {'action' => 'disable', 'name' => 'nginx'})
-  run_task('package', $nodes, {'action' => 'uninstall', 'name' => 'nginx'})
+  run_task('service', $nodes, {'action' => 'stop', 'name' => 'nginx'}, 'become' => true)
+  run_task('service', $nodes, {'action' => 'disable', 'name' => 'nginx'}, 'become' => true)
+  run_task('package', $nodes, {'action' => 'uninstall', 'name' => 'nginx'}, 'become' => true)
 }
