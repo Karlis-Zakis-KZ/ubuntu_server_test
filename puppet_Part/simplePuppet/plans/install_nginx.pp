ed4103024182b458666728c4624219acae79fba3
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2024-05-24 02:26:54 +0300

    25

--- puppet_Part/simplePuppet/plans/install_nginx.pp
+++ puppet_Part/simplePuppet/plans/install_nginx.pp
@@ -1,30 +1,7 @@
 plan simple_puppet_bolt::install_nginx (
   TargetSpec $nodes,
 ) {
-  bolt::task { 'install_nginx':
-    targets => $nodes,
-    task    => 'package',
-    params  => {
-      action => 'install',
-      name   => 'nginx',
-    },
-  }
-
-  bolt::task { 'start_nginx':
-    targets => $nodes,
-    task    => 'service',
-    params  => {
-      action => 'start',
-      name   => 'nginx',
-    },
-  }
-
-  bolt::task { 'enable_nginx':
-    targets => $nodes,
-    task    => 'service',
-    params  => {
-      action => 'enable',
-      name   => 'nginx',
-    },
-  }
+  run_task('package', $nodes, {'action' => 'install', 'name' => 'nginx'})
+  run_task('service', $nodes, {'action' => 'start', 'name' => 'nginx'})
+  run_task('service', $nodes, {'action' => 'enable', 'name' => 'nginx'})
 }
--- puppet_Part/simplePuppet/plans/revert_nginx.pp
+++ puppet_Part/simplePuppet/plans/revert_nginx.pp
@@ -1,30 +1,7 @@
 plan simple_puppet_bolt::remove_nginx (
   TargetSpec $nodes,
 ) {
-  bolt::task { 'stop_nginx':
-    targets => $nodes,
-    task    => 'service',
-    params  => {
-      action => 'stop',
-      name   => 'nginx',
-    },
-  }
-
-  bolt::task { 'disable_nginx':
-    targets => $nodes,
-    task    => 'service',
-    params  => {
-      action => 'disable',
-      name   => 'nginx',
-    },
-  }
-
-  bolt::task { 'remove_nginx':
-    targets => $nodes,
-    task    => 'package',
-    params  => {
-      action => 'uninstall',
-      name   => 'nginx',
-    },
-  }
+  run_task('service', $nodes, {'action' => 'stop', 'name' => 'nginx'})
+  run_task('service', $nodes, {'action' => 'disable', 'name' => 'nginx'})
+  run_task('package', $nodes, {'action' => 'uninstall', 'name' => 'nginx'})
 }
